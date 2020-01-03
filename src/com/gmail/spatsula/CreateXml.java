2a82e83d0d0f7fd5299217f952562a6cdef97b2b
spatsula <spatsula@corelogic.com>
2020-01-03 13:42:12 +0200

    create XML using DOM

---
+++ src/com/gmail/spatsula/CreateXml.java
@@ -0,0 +1,13 @@
+package com.gmail.spatsula;
+
+public class CreateXml {
+    public String createDOM(){
+        String fileName = null;
+        try{
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return fileName;
+    }
+}
