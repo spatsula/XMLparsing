6448c290dd9571513d4c3d7c6de6b327e5bdf7f3
spatsula <spatsula@corelogic.com>
2020-01-03 14:35:23 +0200

    XML DOM parser create

--- src/com/gmail/spatsula/Services/ParserXML.java
+++ src/com/gmail/spatsula/Services/ParserXML.java
@@ -2,10 +2,20 @@ package com.gmail.spatsula.Services;
 
 import com.gmail.spatsula.Entity.Food;
 
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.*;
 import java.util.List;
 
 public class ParserXML {
     public List<Food> parseXLM(String filePath){
+        try{
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
         return null;
     }
 }
